01e2efa7459d3b34add657d367f684e81a813ceb
evertoch <evertoch@outlook.com>
2022-06-23 16:49:44 -0300

    Ajustado os campos Not Null do mapa relacional, bem como no create.

--- Scripts/Script_create_Table_DataCine.sql
+++ Scripts/Script_create_Table_DataCine.sql
@@ -1,34 +1,34 @@
 CREATE TABLE Cinema (
   codcin SERIAL NOT NULL, 
-  nomcin varchar(40), 
+  nomcin varchar(40) NOT NULL, 
   PRIMARY KEY (codcin));
- 
+  
 COMMENT ON TABLE Cinema IS 'Coluna para cadastro do cinema        ';
 COMMENT ON COLUMN Cinema.codcin IS 'Coluna para código do cinema';
 COMMENT ON COLUMN Cinema.nomcin IS 'Coluna para cadastro do nome do cinema';
 
 CREATE TABLE diretor (
   iddir  SERIAL NOT NULL, 
-  nomdir varchar(45), 
+  nomdir varchar(45) NOT NULL, 
   PRIMARY KEY (iddir));
- 
+  
 COMMENT ON COLUMN diretor.iddir IS 'Identificador do cadastro de diretores';
 COMMENT ON COLUMN diretor.nomdir IS 'Tabela para cadastro do nome do diretor';
 
 CREATE TABLE filme (
   idfilm    SERIAL NOT NULL, 
-  nomBR     varchar(45), 
-  nomEN     varchar(45) NOT NULL, 
-  anolanfan int4, 
-  sinfil    varchar(255), 
-  dubleg    int4 DEFAULT 0, 
-  durfil    time, 
-  claind    int4, 
-  mesexi    int4, 
+  nomBR     varchar(45) NOT NULL, 
+  nomEN     varchar(45), 
+  anolanfan int4 NOT NULL, 
+  sinfil    varchar(255) NOT NULL, 
+  dubleg    int4 DEFAULT 0 NOT NULL, 
+  durfil    time NOT NULL, 
+  claind    int4 NOT NULL, 
+  mesexi    int4 NOT NULL, 
   iddir     int4 NOT NULL, 
   idgen     int4 NOT NULL, 
   PRIMARY KEY (idfilm));
- 
+  
 COMMENT ON TABLE filme IS 'Tabela para cadastro do filme';
 COMMENT ON COLUMN filme.idfilm IS 'Identificador do filme';
 COMMENT ON COLUMN filme.nomBR IS 'Nome do filme no Brasil';
@@ -40,36 +40,33 @@ COMMENT ON COLUMN filme.durfil IS 'Controle da duração do filme, em horas.';
 COMMENT ON COLUMN filme.claind IS 'Classificação indicativa de idade para o filme';
 COMMENT ON COLUMN filme.mesexi IS 'Mês de exibição do filme. Iniciando em 1 - Janeiro e 12 - Dezembro.';
 
-CREATE TABLE ingresso (
-  iding  SERIAL NOT NULL, 
-  datven date NOT NULL, 
-  idfilm int4 NOT NULL, 
-  valing int4, 
-  PRIMARY KEY (iding));
- 
-COMMENT ON TABLE ingresso IS 'Coluna para registrar os ingressos vendidos';
-COMMENT ON COLUMN ingresso.iding IS 'Id do ingresso';
-COMMENT ON COLUMN ingresso.datven IS 'Data da venda do ingresso';
-COMMENT ON COLUMN ingresso.idfilm IS 'Id do filme que o ingresso vale';
-COMMENT ON COLUMN ingresso.valing IS 'Valor cobrado pelo ingresso.';
+CREATE TABLE filme_premiacao (
+  idpre      int4 NOT NULL, 
+  idfilm     int4 NOT NULL, 
+  gan        int4 DEFAULT 0 NOT NULL, 
+  id_fil_pre SERIAL NOT NULL, 
+  PRIMARY KEY (id_fil_pre));
+  
+COMMENT ON COLUMN filme_premiacao.gan IS 'Flag para definir se ganhou ou não a inidicação';
+COMMENT ON COLUMN filme_premiacao.id_fil_pre IS 'Chave primária';
 
 CREATE TABLE funcao (
   idfun  SERIAL NOT NULL, 
-  nomfun varchar(45), 
+  nomfun varchar(45) NOT NULL, 
   PRIMARY KEY (idfun));
- 
+  
 COMMENT ON TABLE funcao IS 'Tabela de cadastro das funções do funcionário';
 COMMENT ON COLUMN funcao.idfun IS 'Identificador da função';
 COMMENT ON COLUMN funcao.nomfun IS 'Nome da função';
 
 CREATE TABLE funcionario (
   cartrab SERIAL NOT NULL, 
-  nomfun  varchar(45), 
-  datadm  date, 
-  salfun  float4, 
+  nomfun  varchar(45) NOT NULL, 
+  datadm  date NOT NULL, 
+  salfun  float4 NOT NULL, 
   codcin  int4 NOT NULL, 
   PRIMARY KEY (cartrab));
- 
+  
 COMMENT ON TABLE funcionario IS 'Tabela de cadastro de funcionários';
 COMMENT ON COLUMN funcionario.cartrab IS 'Identificador do funcionário (Nº da carteira de trabalho)';
 COMMENT ON COLUMN funcionario.nomfun IS 'Nome do funcionário';
@@ -78,18 +75,18 @@ COMMENT ON COLUMN funcionario.salfun IS 'Salário do funcionário';
 
 CREATE TABLE genero (
   idgen  SERIAL NOT NULL, 
-  nomgen varchar(45), 
+  nomgen varchar(45) NOT NULL, 
   PRIMARY KEY (idgen));
- 
+  
 COMMENT ON TABLE genero IS 'Coluna para cadastro dos gêneros de filmes';
 COMMENT ON COLUMN genero.idgen IS 'Identificador do genero';
 COMMENT ON COLUMN genero.nomgen IS 'Coluna especificadora do nome do gênero';
 
 CREATE TABLE horario (
   idhor SERIAL NOT NULL, 
-  hor   time(7), 
+  hor   time(7) NOT NULL, 
   PRIMARY KEY (idhor));
- 
+  
 COMMENT ON TABLE horario IS 'Tabela para cadastro dos horários já pré-definidos';
 COMMENT ON COLUMN horario.idhor IS 'Identificador do horário';
 COMMENT ON COLUMN horario.hor IS 'Hora de exibição';
@@ -100,15 +97,28 @@ CREATE TABLE horario_funcionario (
   idfuncao      int4 NOT NULL, 
   id_hor_fun    SERIAL NOT NULL, 
   PRIMARY KEY (id_hor_fun));
- 
+  
 COMMENT ON COLUMN horario_funcionario.id_hor_fun IS 'Chave primária';
+
+CREATE TABLE ingresso (
+  iding  SERIAL NOT NULL, 
+  datven date NOT NULL, 
+  idfilm int4 NOT NULL, 
+  valing numeric(5, 2) NOT NULL, 
+  PRIMARY KEY (iding));
+  
+COMMENT ON TABLE ingresso IS 'Coluna para registrar os ingressos vendidos';
+COMMENT ON COLUMN ingresso.iding IS 'Id do ingresso';
+COMMENT ON COLUMN ingresso.datven IS 'Data da venda do ingresso';
+COMMENT ON COLUMN ingresso.idfilm IS 'Id do filme que o ingresso vale';
+COMMENT ON COLUMN ingresso.valing IS 'Valor cobrado pelo ingresso.';
 
 CREATE TABLE premiacao (
   idpre  SERIAL NOT NULL, 
-  nompre varchar(45), 
-  anopre int4, 
+  nompre varchar(45) NOT NULL, 
+  anopre int4 NOT NULL, 
   PRIMARY KEY (idpre));
- 
+  
 COMMENT ON TABLE premiacao IS 'Tabela responsável pelo cadastro das premiações';
 COMMENT ON COLUMN premiacao.idpre IS 'Coluna do ID da premiação';
 COMMENT ON COLUMN premiacao.nompre IS 'Nome da premiação';
@@ -119,7 +129,7 @@ CREATE TABLE sala (
   nomsal varchar(45), 
   capsal int4, 
   PRIMARY KEY (idsal));
- 
+  
 COMMENT ON TABLE sala IS 'Tabela para cadastro das salas de cinema';
 COMMENT ON COLUMN sala.idsal IS 'Identificador da sala de cinema';
 COMMENT ON COLUMN sala.nomsal IS 'Nome da sala de cinema';
@@ -131,18 +141,8 @@ CREATE TABLE sala_filme (
   idhor      int4 NOT NULL, 
   id_sal_fil SERIAL NOT NULL, 
   PRIMARY KEY (id_sal_fil));
- 
+  
 COMMENT ON COLUMN sala_filme.id_sal_fil IS 'Chave primária';
-
-CREATE TABLE filme_premiacao (
-  idpre      int4 NOT NULL, 
-  idfilm     int4 NOT NULL, 
-  gan        int4 DEFAULT 0, 
-  id_fil_pre SERIAL NOT NULL, 
-  PRIMARY KEY (id_fil_pre));
- 
-COMMENT ON COLUMN filme_premiacao.gan IS 'Flag para definir se ganhou ou não a inidicação';
-COMMENT ON COLUMN filme_premiacao.id_fil_pre IS 'Chave primária';
 
 ALTER TABLE funcionario ADD CONSTRAINT FUNCIONARIO_FK FOREIGN KEY (codcin) REFERENCES Cinema (codcin);
 ALTER TABLE filme ADD CONSTRAINT FILME_FK FOREIGN KEY (idgen) REFERENCES genero (idgen);
