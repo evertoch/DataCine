bcca565af387fe947a6c8df09818e078efc3b329
evertoch <evertoch@outlook.com>
2022-11-25 00:31:14 -0300

    Adicionado a segunda Triggers.sql

--- Banco de Dados II/Scripts/Triggers.sql
+++ Banco de Dados II/Scripts/Triggers.sql
@@ -35,3 +35,42 @@ on filme
 for each row
 execute procedure filme_log_func();
 
+
+-- Trigger 02:
+-- Controle de assentos vendidos de acordo com a capacidade da sala que está exibindo o filme.
+-- Funcionamento: O sistema irá somar a quantidade de ingressos vendidos e irá comparar com a capacidade da sala que o filme está sendo exibido.
+-- Caso o total de ingresos fique igual a capacidade da sala, o sistema trará a mensagem de erro.
+
+-- Criação da Procedure:
+
+create or replace function verifica_disponibilidade()
+returns trigger
+as
+$body$
+declare
+	conta numeric;
+	capacidade numeric;
+begin
+	select count(*) into conta from ingresso i 
+	inner join sala_filme sf on i.id_sal_fil = sf.id_sal_fil
+	inner join sala s on sf.idsala = s.idsal
+	where i.id_sal_fil = new.id_sal_fil;
+	select capsal into capacidade from sala s
+	inner join sala_filme sf on s.idsal = sf.idsala
+	inner join ingresso i on sf.id_sal_fil = i.id_sal_fil
+	where i.id_sal_fil = new.id_sal_fil
+	group by capsal;
+		if (conta >= capacidade) then
+		raise exception 'Não há mais assentos disponíveis para a sessão atual'; 
+	end if;
+	return new;
+end
+$body$
+language plpgsql;
+
+-- Criação da Trigger:
+
+create trigger disponibilidade_before_tg
+before insert on ingresso
+for each row
+execute procedure verifica_disponibilidade();
